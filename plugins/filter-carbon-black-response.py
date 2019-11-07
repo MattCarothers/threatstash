7d0cd2122c852647bdeba5e344c079119ead75be
Matt Carothers <matt.carothers@cox.com>
2019-11-07 16:24:23 -0500

    Initial commit for Carbon Black Response API query

---
+++ plugins/filter-carbon-black-response.py
@@ -0,0 +1,62 @@
+import threatstash.plugin
+
+# Plugin to query the Carbon Black Response API
+
+__PLUGIN_NAME__ = 'filter-carbon-black-response'
+__PLUGIN_TYPE__ = 'filter'
+__IOC_TYPES__ = [ 'ipv4-addr', 'md5', 'sha256' ]
+__REQUIRED_PARAMETERS__ = [ ]
+
+import cbapi.response as cb
+import cbapi.errors
+
+class CBRFilter(threatstash.plugin.Plugin):
+    def __init__(self, config = {}):
+        super().__init__(__PLUGIN_NAME__, __PLUGIN_TYPE__, __IOC_TYPES__, __REQUIRED_PARAMETERS__, config)
+        self.cbr = cb.CbResponseAPI()
+
+    def run(self, event):
+        for observable in event.observables:
+            # Observable is an IP?
+            if observable.type == 'ipv4-addr':
+                # Issue a CB process query
+                query = 'ipaddr:' + observable.value
+                processes = self.cbr.select(cb.Process).where(query)
+                count = len(processes)
+                if count > 0:
+                    last_seen = processes.first().last_update
+                    event.add_sighting(
+                        observable.id,
+                        last_seen=last_seen,
+                        sighted_by='cbr',
+                        # The webui link defaults to 0 rows, so we have to
+                        # specify a number here in order to see results in a
+                        # browser
+                        refs=['cbr-url', processes.webui_link + '&rows=10'],
+                        count=count
+                    )
+                    self.debug("sighted", observable.value, "at",
+                            str(last_seen))
+            else:
+                # This observable is a file hash
+                try:
+                    binary = self.cbr.select(cb.Binary, observable.value)
+                    event.add_sighting(
+                        observable.id,
+                        last_seen=binary.last_seen,
+                        sighted_by='cbr',
+                        refs=['cbr-url', binary.webui_link]
+                    )
+                    self.debug("sighted", observable.value, "at",
+                            binary.last_seen)
+                except cbapi.errors.ObjectNotFoundError as e:
+                    self.debug(observable.value, "not found")
+                    continue
+                except Exception as e:
+                    self.debug(
+                        "Unable to retrieve binary information from CBR"
+                    )
+                    self.debug(repr(e))
+                    continue
+
+        return event
